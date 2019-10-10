84b1280d494b8f54874c2a910fe629bc6dbf0749
Marven Mathelier <marven250@hotmail.com>
2019-10-10 14:08:58 -0400

    progresso

---
+++ client/src/components/pages/Secret.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import api from '../../api'
+import funnyMemes from '../../funnyMemes'
+
+export default class Secret extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showMeme: false,
+    }
+  }
+
+  displayMeme = () => {
+    if (this.state.showMeme) {
+      let randNum = Math.floor(Math.random() * funnyMemes.length)
+      let found = funnyMemes.find(meme => {
+        return meme.idx === randNum
+      })
+      console.log(found)
+
+      return (
+        <div>
+          <img id="funnyPic" src={found.picAdress} alt="test" />
+        </div>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>
+          <button
+            id="funnyButton"
+            onClick={() => this.setState({ showMeme: !this.state.showMeme })}
+          >
+            Generate Car Funny
+          </button>
+        </h2>
+        {this.displayMeme()}
+      </div>
+    )
+  }
+}
